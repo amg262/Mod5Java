8257ef8e44251bb832dd33ee58e20d0858654d41
Andrew <andrewmgunn26@gmail.com>
2022-02-15 15:22:21 -0600

    ALMOST

--- src/edu/wctc/Main.java
+++ src/edu/wctc/Main.java
@@ -1,8 +1,26 @@
 package edu.wctc;
 
+import java.time.*;
+
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        LocalDate l1 = LocalDate.of(1987, 3, 9);
+        LocalDate l3 = LocalDate.of(1988, 10, 10);
+        LocalDate l2 = LocalDate.now();
+        Period yearsApart = Period.between(l1, l2);
+        Period yearsApart2 = Period.between(l1, l3);
+       // l3.plus()
+
+        Period p = Period.of(1,7,1);
+
+        System.out.printf("%d years, %d months, and %d days ago", yearsApart.getYears(), yearsApart.getMonths(), yearsApart.getDays());
+        System.out.println();
+        System.out.printf("%d years, %d months, and %d days ago", yearsApart2.getYears(), yearsApart2.getMonths(), yearsApart2.getDays());
+        System.out.println(p.getYears());
     }
 }
